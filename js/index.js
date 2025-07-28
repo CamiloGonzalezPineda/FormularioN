f49d89fc7914ba7435fbf8d8547cfa3eb22e1230
CamiloGonzalezPineda <camgp98@gmail.com>
2025-07-28 15:30:01 -0500

    Actualización 28/07

--- js/index.js
+++ js/index.js
@@ -1,38 +1,39 @@
 const formulario = document.querySelector('form')
 const btn = document.querySelector('.btn')
-const inputs = document.querySelectorAll('input,textarea')
-window.addEventListener('pageshow', ()=>{
+const inputs = document.querySelectorAll('.texto')
+
+window.addEventListener('pageshow', () => {
     formulario.reset()
 })
 
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
+   
 
-let mostrar = false
+    let mostrar = false
 
-             const mensajeE = document.querySelector('.red')
-             if(mensajeE){mensajeE.remove()}
+    const mostraE = document.querySelector('.red')
+    if (mostraE) { mostraE.remove() }
 
-inputs.forEach(bt => {
-   if(bt.value === ""){ // evalua que este vacio
-    mostrar= true // muestra el boton
-   }
-});
-             if(mostrar){
+    inputs.forEach(inp => {
+        if (inp.value.trim() === "") {
+            mostrar = true
+        }
+    });
 
-                const ale= document.createElement('div') // crea un div 
-                ale.classList.add('red') // da un clase al div
-                const tex = document.createElement('p') // crea un parrafo 
-                tex.textContent = "Completa los campos" // le agrega texto al parrafo 
-                
-                ale.appendChild(tex) // crea los bottones 
-                formulario.appendChild(ale) // cre el boton
+    if (mostrar) {
 
-                  setTimeout(() => {
-    ale.remove()
-  }, 1000);
+        const ale = document.createElement('div') // crea un div 
+        ale.classList.add('red') // da un clase al div
+        const tex = document.createElement('p') // crea un parrafo 
+        tex.textContent = "Completa los campos" // le agrega texto al parrafo 
 
- 
-            }
+        ale.appendChild(tex) // crea los bottones 
+        formulario.appendChild(ale) // cre el boton
+
+        setTimeout(() => {
+            ale.remove()
+        }, 1000);
+    }
+
 })
-
